b8acd436eae8808656e3f673566edcb9078f6bef
MMik1 <m.mik@sintlucasedu.nl>
2026-06-17 10:41:37 +0200

    Update App.vue

--- Project/Opdracht_1/src/App.vue
+++ Project/Opdracht_1/src/App.vue
@@ -14,13 +14,13 @@ import zomerboom from './assets/zomerboomimage.png'
 import herstboom from './assets/herfstboomimage.png'
 
 import zomerAchtergrond from './assets/Achtergrond-zomer.png'
-import herfstAchtergrond from './assets/Achtergrond-lentev2.png'
+import herfstAchtergrond from './assets/Achtergrond-herfst.png'
 import winterAchtergrond from './assets/Achtergrond-winter.png'
 import lenteAchtergrond from './assets/Achtergrond-lentev2.png'
-
 const waterHeight = ref(0)
 const grondwaterRef = ref(null)
 const sliderRef = ref(null)
+const MAX_WATER_PERCENT = 70
 
 let resetTimer
 
@@ -54,7 +54,7 @@ function startResetTimer() {
 }
 
 function updateRain(value) {
-  waterHeight.value = (value / 190) * 600
+  waterHeight.value = (value / 190) * MAX_WATER_PERCENT
 
   grondwaterRef.value?.veranderGrondwaterDoorSlider(value)
 
@@ -64,7 +64,7 @@ function updateRain(value) {
 function updateTemperature(value) {
   const omgekeerdeWaarde = 190 - value
 
-  waterHeight.value = (omgekeerdeWaarde / 190) * 600
+  waterHeight.value = (omgekeerdeWaarde / 190) * MAX_WATER_PERCENT
 
   grondwaterRef.value?.veranderGrondwaterDoorSlider(omgekeerdeWaarde)
 
@@ -73,142 +73,109 @@ function updateTemperature(value) {
 </script>
 
 <template>
-  <div >
-<!--    id="background" :style="{ backgroundImage: `url(${backgroundImage})` }"-->
+  <div id="mainContent">
+    <img class="bgLayer" :src="backgroundImage" alt="" />
     <div id="Header">
       <Header />
     </div>
-
-    <div id="mainContent">
-      <div id="tree">
-        <img :src="treeImage" alt="Seizoensboom">
-
-        <div id="grondwaterkastje">
-          <Grondwaterstand ref="grondwaterRef" />
-        </div>
-      </div>
-
-      <div id="grass"></div>
-      <div id="dirt"></div>
-
-      <div
-        id="water"
-        :style="{ height: `${waterHeight}px` }"
-      ></div>
-
-      <div id="sliders">
-        <WhatsHappening
-          ref="sliderRef"
-          @rain="updateRain"
-          @temperature="updateTemperature"
-        />
-      </div>
+    <div id="water" :style="{ height: waterHeight + '%' }"></div>
+    <div id="tree">
+      <img :src="treeImage" alt="Seizoensboom" />
     </div>
-
-    <div id="Quiz">
-      <Quiz />
+    <div id="grondwaterkastje">
+      <Grondwaterstand ref="grondwaterRef" />
     </div>
-
-    <div id="qrcode">
-      <qrcode />
+    <div id="sliders">
+      <WhatsHappening
+        ref="sliderRef"
+        @rain="updateRain"
+        @temperature="updateTemperature"
+      />
     </div>
+  </div>
 
-    <div id="footer">
-      <Footer />
-    </div>
+  <div id="Quiz">
+    <Quiz />
+  </div>
+
+  <div id="qrcode">
+    <qrcode />
+  </div>
+
+  <div id="footer">
+    <Footer />
   </div>
 </template>
 
 <style scoped>
+
 #mainContent {
   position: relative;
+  width: 100%;
   overflow: hidden;
 }
 
+.bgLayer {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+#tree {
+  position: absolute;
+  left: 50%;
+  bottom: 69%;                
+  transform: translateX(-50%);
+  width: 22%;               
+  z-index: 4;
+}
+
+#tree img {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+#grondwaterkastje {
+  position: absolute;
+  left: 2%;
+  bottom: 69%;                 /* op de grasrand */
+  width: clamp(8rem, 15%, 15rem);
+  height: clamp(11rem, 20%, 20rem);
+  background-color: gray;
+  z-index: 20;
+}
 #sliders {
   position: absolute;
-  top: 650px;
+  left: 50%;
+  top: 4%;                  
+  transform: translateX(-50%);
   width: 100%;
   display: flex;
   justify-content: center;
   z-index: 30;
 }
-
-#tree {
-  position: relative;
-  display: flex;
-  justify-content: center;
-  z-index: 3;
-  margin-bottom: -15px;
-}
-
-#tree img {
-  width: 400px;
-  height: 500px;
-}
-
-
-#grondwaterkastje {
-  position: absolute;
-  left: 2vw;
-  top: calc(500px - 20rem);
-  width: 15rem;
-  height: 20rem;
-  background-color: gray;
-  z-index: 20;
-}
-
-#grass {
-  position: relative;
-  width: 100%;
-  height: 100px;
-  background-color: #598F29;
-  z-index: 2;
-}
-
-#dirt {
-  position: relative;
-  width: 100%;
-  height: 600px;
-  background-color: #7C6034;
-  z-index: 2;
-}
-
-#backgroundimage{
-  inset: 0;
-  z-index: 1;
-}
-
-//#backgroundimage img{
-//  width: 100%;
-//  height: 100%;
-//  object-fit: cover;
-//  display: block;
-//}
-
-//#background{
-//  position: relative;
-//  background-size: cover;
-//  background-position: center;
-//  background-repeat: no-repeat;
-//  width: 100%;
-//}
-
-
 #water {
   position: absolute;
+  left: 0;
   bottom: 0;
   width: 100%;
-  background-color: rgba(11, 132, 183, 0.75);
-  z-index: 3;
-  max-height: 600px;
-  min-height: 0;
+  background-color: rgba(11, 132, 183, 0.55);
+  z-index: 1;
   pointer-events: none;
-  transition: height .3s ease;
+  transition: height 0.3s ease;
+}
+
+#Header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 40;         
+
 }
 
 #footer {
   margin-top: 64px;
 }
-
 </style>
